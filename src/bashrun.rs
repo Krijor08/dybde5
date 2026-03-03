b88a44b760a3a087f7e1115cfca39ae2f785cbd5
Krijor08 <krijor08@gmail.com>
2026-03-03 12:18:22 +0100

    seriously honestly really actually still trying to fix run_ip_script

--- src/bashrun.rs
+++ src/bashrun.rs
@@ -2,25 +2,40 @@ use std::process::{Command};
 use std::path::Path;
 use std::io;
 
+use logger::Message;
+use crate::logger;
+
 pub fn run_ip_script() -> io::Result<()> {
 	let script_path = Path::new("scripts/ipscan");
 	if !script_path.exists() {
-		eprintln!("Script not found: {}", script_path.display());
-		return Ok(());
+		logger(&Message {
+			content: format!("Script not found at path: {}", script_path.display()),
+			level: 400,
+		});
 	} else {
-		println!("Running script: {}", script_path.display());
+		logger(&Message {
+			content: format!("Running script at path: {}", script_path.display()),
+			level: 102,
+		});
 	}
 
-	let output = Command::new(format!("sudo bash {}", script_path.display()))
+	let output = Command::new(format!("bash"))
+		.arg(script_path.display().to_string())
 		.arg("arg1")
 		.arg("arg2")
 		.arg("arg3")
 		.output()?;
 
 	if output.status.success() {
-		println!("Script output:\n{}", String::from_utf8_lossy(&output.stdout));
+		logger(&Message {
+			content: format!("Script output:\n{}", String::from_utf8_lossy(&output.stdout)),
+			level: 200,
+		});
 	} else {
-		eprintln!("Script failed with error:\n{}", String::from_utf8_lossy(&output.stderr));
+		logger(&Message {
+			content: format!("Script failed with error:\n{}", String::from_utf8_lossy(&output.stderr)),
+			level: 500,
+		});
 	}
 
 	Ok(())
