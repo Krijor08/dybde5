50dbcd2bef3f4f2c87c01e2ed8b0468a709d9817
Krijor08 <krijor08@gmail.com>
2026-03-20 12:59:51 +0100

    Improved help

--- src/help.rs
+++ src/help.rs
@@ -1,15 +1,17 @@
 pub fn help() {
-	println!("Available commands:");
-	println!("ip - Validate an IP address");
-	println!("login (or l) - Log in to your account");
-	println!("signup (or su) - Create a new user account");
-	println!("script (or s) - Run a predefined script (Linux only)");
-	println!("help (or h) - Show this help message");
-	println!("exit (or e) - Exit the program");
+	println!("\n---------------- HELP ----------------\n");
+	println!("Available commands:\n");
+	println!("-  ip _____________ Validate an IP address");
+	println!("-  login, l ___ Log in to your account");
+	println!("-  signup, su _ Create a new user account");
+	println!("-  editaccesslevel, eal ___ Edit a user's access level (admin only)");
+	println!("-  script, s __ Run a predefined script (Linux only)");
+	println!("-  help, h ____ Show this help message");
+	println!("-  exit, e ____ Exit the program");
 }
 
 pub fn login_help() {
-	println!("\nLogin Help:\n");
+	println!("\n---------------- LOGIN HELP ----------------\n");
 	println!("Enter your username when prompted.");
 	println!("Enter your password when prompted.");
 	println!("If you don't have an account, type 'signup' or 'su' to create one.");
